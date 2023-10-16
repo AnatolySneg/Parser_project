8305ae08cc5c7ad6d51e00a96a3ef4d5e2ad9287
AnatolySneg <AnatolySneg@gmail.com>
2023-10-16 18:55:11 +0300

    Corrected path to migrations folder in alembic.ini, made a migration, but didn't migrated it yet

---
+++ src/auth/database.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase, SQLAlchemyBaseUserTable
+from sqlalchemy import String, Boolean, Integer, TIMESTAMP, ForeignKey
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from config import DB_NAME, DB_HOST, DB_PORT, DB_PASSWORD, DB_USER
+from src.models.models import user_status
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+# DATABASE_URL = f"postgresql+asyncpg://user:password@host:port/name"
+# DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    # Columns from models:
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    phone_number: Mapped[str] = mapped_column(String, nullable=False)
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+    registered_at = mapped_column(TIMESTAMP, default=datetime.utcnow)
+    user_status_id = mapped_column(Integer, ForeignKey(user_status.c.id))
+
+    # Default fastapi_users.db columns:
+    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+        # TODO: """from fastapi-users documentation
+        #  https://fastapi-users.github.io/fastapi-users/12.1/configuration/databases/sqlalchemy/
+        #  In production, it's strongly recommended to setup a migration system to update your SQL schemas.
+        #  See Alembic. https://alembic.sqlalchemy.org/en/latest/"""
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
--- src/models/models.py
+++ src/models/models.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 
 metadata = MetaData()
 
 user_status = Table(
     "user_status",
     metadata,
-    Column("id", Integer, primary_key=True),
+    Column("id", Integer, unique=True, primary_key=True),
     Column('name', String, nullable=False),
     Column("permissions", JSON),
 )
@@ -16,14 +16,19 @@ user_status = Table(
 user = Table(
     "user",
     metadata,
-    Column("id", Integer, primary_key=True),
-    Column('email', String, nullable=False),
+    Column("id", Integer, unique=True, primary_key=True),
+    Column('email', String(length=320), unique=True, index=True, nullable=False),
     Column('phone_number', String, nullable=False),
     Column('username', String, nullable=False),
     Column('first_name', String(50)),
     Column('last_name', String(50)),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("user_status_id", Integer, ForeignKey("user_status.id")),
+    Column("user_status_id", Integer, ForeignKey(user_status.c.id)),
+
+    Column("hashed_password", String(length=1024), nullable=False),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
 
 
